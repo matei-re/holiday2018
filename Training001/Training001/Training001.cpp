902a09c67eb4dce6ea4812902f78403068b58946
Raluca Matei <re.matei@gmail.com>
2018-06-23 16:04:43 +0300

    Divizorii unui numar x

--- Training001/Training001/Training001.cpp
+++ Training001/Training001/Training001.cpp
@@ -2,10 +2,20 @@
 //
 
 #include "stdafx.h"
-
+#include <iostream>
+using namespace std;
 
 int main()
 {
+	int x, divi;
+	cin >> x;
+	for (divi = 1; divi <= x; divi++) 
+	{
+		if (x % divi == 0)
+		{
+			cout << divi << " ";
+		}
+	}
     return 0;
 }
 
