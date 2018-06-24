c93beee0f9cc7d0c3263b407b71d58ce06abc0d2
Raluca Matei <re.matei@gmail.com>
2018-06-24 11:06:19 +0300

    Suma cifrelor terminat

--- training002/training002/training002.cpp
+++ training002/training002/training002.cpp
@@ -2,10 +2,21 @@
 //
 
 #include "stdafx.h"
-
+#include <iostream>
+using namespace std;
 
 int main()
 {
+	int x, s;
+	cin >> x;
+	s = 0;
+	while (x != 0)
+	{
+		s += (x % 10);
+		x = x / 10;
+	}
+	cout << "Suma cifrelor numaului dat este de " << s << endl;
+
     return 0;
 }
 
