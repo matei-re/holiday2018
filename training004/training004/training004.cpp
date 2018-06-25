d252c8ee11743dc5e130787da3f561951d7813d7
Raluca Matei <re.matei@gmail.com>
2018-06-25 11:10:36 +0300

    toate nr mai mici decat ... in baza ...

--- training004/training004/training004.cpp
+++ training004/training004/training004.cpp
@@ -7,21 +7,24 @@ using namespace std;
 
 int main()
 {
-	int n, k, x, p;
+	int n, m, k, x, p;
 	cout << "Introdu nr!" << endl;
-	cin >> n;
+	cin >> m;
 	cout << "Introdu baza!" << endl;
 	cin >> k;
-	x = 0; // x o sa fie n in baza k
-	p = 1; // puterea (1, 10, 100, ...)
-	while (n)
+	for (int i = 1; i <= m; i++)
 	{
-		x = n % k * p + x;
-		p *= 10;
-		n = n / k;
+		n = i;
+		x = 0; // x o sa fie n in baza k
+		p = 1; // puterea (1, 10, 100, ...)
+		while (n)
+		{
+			x = n % k * p + x;
+			p *= 10;
+			n = n / k;
+		}
+		cout << "Nr " << i << " in baza " << k << " este " << x << endl;
 	}
-	cout << "Nr " << n << " in baza " << k << " este " << x << endl;
-
     return 0;
 }
 
