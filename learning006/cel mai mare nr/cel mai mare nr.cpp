39bd4c6094ee08b5f8bfd2c926f41f72e8a8a662
Raluca Matei <re.matei@gmail.com>
2018-06-26 12:44:00 +0300

    cel mai mare nr din cifrele din cifrele lui n

--- learning006/cel mai mare nr/cel mai mare nr.cpp
+++ learning006/cel mai mare nr/cel mai mare nr.cpp
@@ -1,11 +1,36 @@
-// cel mai mare nr.cpp : Defines the entry point for the console application.
-//
+// cel mai mare nr.cpp : Defines the entry point for the console 
+// application.
+
+// Se da un nr. Sa se afiseze  cel mai mare nr care se poate forma 
+// din cifrele sale. 
 
 #include "stdafx.h"
-
+#include <iostream>
+using namespace std;
 
 int main()
 {
+	int n, m, x, o;
+
+	cout << "Nr." << endl;
+	cin >> n;
+
+	m = 0;
+
+	for (x = 9; x >= 0; x--)
+	{
+		o = n;
+		while (o)
+		{
+			if (x == o % 10)
+			{
+				m = m * 10 + x;
+			}
+			o /= 10;
+		}
+	}
+	
+	cout << "Cel mai mare nr  format cu cifrele lui " << n << " este " << m << endl;
+
     return 0;
 }
-
