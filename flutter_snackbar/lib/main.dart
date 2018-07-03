428f0a30c6d4b9d5e916e9c1c7734b5a3c445e97
Raluca Matei <re.matei@gmail.com>
2018-07-03 14:23:52 +0300

    test1

---
+++ flutter_snackbar/lib/main.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+//
+void main() => runApp(SnackBarDemo());
+
+class SnackBarDemo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'SnackBar Demo',
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        primaryColor: Colors.orange,
+        accentColor: Colors.deepOrange,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('REM SnackBar Demo'),
+        ),
+        body: SnackBarPage(),
+      ),
+    );
+  }
+}
+
+class SnackBarPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+
+      child: RaisedButton(
+
+        onPressed: () {
+          final snackBar = SnackBar(
+            content: Text('Yay! A SnackBar!'),
+            action: SnackBarAction(
+              label: 'Undo',
+              onPressed: () {
+                // Some code to undo the change!
+              },
+            ),
+          );
+
+          // Find the Scaffold in the Widget tree and use it to show a SnackBar!
+          Scaffold.of(context).showSnackBar(snackBar);
+        },
+        child: Text('Show SnackBar'),
+      ),
+    );
+  }
+}
